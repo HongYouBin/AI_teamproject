80f7cb4cefbf7ba3795af2e2f1016a54075d3701
HongYouBin <ghddbqls33@gmail.com>
2022-05-15 02:31:01 +0900

    modified

--- web_crawling.py
+++ web_crawling.py
@@ -1,38 +1,43 @@
 #출처) 야구기록실 케이비리포트 KBReport.com
 
-import requests
+import requests #pip install requests
 from bs4 import BeautifulSoup #pip install beautifulsoup4
 import re
 import xml.etree.ElementTree as ET
 import sys
 
-sys.stdout = open('output.txt', 'w') 
+#sys.stdout = open('output.txt', 'w') 
 
 lst = []
 
-for i in range(1592, 1593):
+for i in range(1335, 1340):
     url = f"http://www.kbreport.com/player/detail/{i}"
     res = requests.get(url)
+    if res.status_code != 200:
+        continue
     res.raise_for_status()
     soup = BeautifulSoup(res.text, "lxml") #pip install lxml
 
-    tmp = {}
 
     tr = soup.find("tr", attrs={"class": "even"})
-    if tr == None:
+    year = str(tr.find("td").get_text())
+    if tr == None or year != "2021":
         continue
     stat = tr.find_all("td", attrs={"class": "data"})
+
+    tmp = {}
 
     ageLst = soup.find_all("span", attrs={"class":"player-info-1"})
     age = ageLst[1].get_text()
     age = age[3:5]
     money = str(soup.find("span", attrs={"class":"player-info-8"}).get_text())
     if money == ' ':
-        continue
-    idx = money.find('\r')
-    money = money[5:idx]
-    # money = int(re.sub(",", "", money))
-    money = money.replace(',',"")
+        money = 3000
+    else:
+        idx = money.find('\r')
+        money = money[5:idx]
+        # money = int(re.sub(",", "", money))
+        money = money.replace(',',"")
     position = str(soup.find("span", attrs={"class":"player-info-4"}).get_text())
     position = position[3:]
     position = position.replace('\n',"")
@@ -64,10 +69,11 @@ for i in range(1592, 1593):
         tmp['homerun'] = homerun
         tmp['stealing'] = stealing
     lst.append(tmp)
+    #print(tmp)
     
-print(lst)
+#print(lst)
         
-sys.stdout.close()
+#sys.stdout.close()
 
 
 
